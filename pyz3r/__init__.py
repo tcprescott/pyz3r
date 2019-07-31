fdb2cb59bc69f9bfe05fd4bfc217e3836e3c6ea8
Thomas Prescott <tcprescott@gmail.com>
2019-07-31 17:33:07 -0500

    Bump version: 3.0.0 → 3.1.0

--- pyz3r/__init__.py
+++ pyz3r/__init__.py
@@ -8,7 +8,7 @@ __title__ = 'pyz3r'
 __author__ = 'Thomas Prescott'
 __license__ = 'Apache 2.0'
 __copyright__ = 'Copyright 2018-2019 Thomas Prescott'
-__version__ = '3.0.0'
+__version__ = '3.1.0'
 
 from .alttpr import alttpr
 from .rom import romfile
