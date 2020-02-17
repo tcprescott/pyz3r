f84351a7218dc6aa01dea76ec6b050d7e0ccf64a
Thomas Prescott <tcprescott@gmail.com>
2020-02-16 19:38:17 -0600

    Bump version: 4.3.3 → 4.4.0

--- pyz3r/__init__.py
+++ pyz3r/__init__.py
@@ -8,7 +8,7 @@ __title__ = 'pyz3r'
 __author__ = 'Thomas Prescott'
 __license__ = 'Apache 2.0'
 __copyright__ = 'Copyright 2018-2019 Thomas Prescott'
-__version__ = '4.3.3'
+__version__ = '4.4.0'
 
 from .alttpr import alttpr
 from .smz3 import smz3
