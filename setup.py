3db55c0749de0b603c091b13ad8b5b35489c9b1f
Thomas Prescott <tcprescott@gmail.com>
2020-08-01 13:21:59 -0500

    whelp

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ setup(
     install_requires=[
         'aiofiles',
         'aiohttp',
-        'python-bps @ https://github.com/tcprescott/python-bps/tarball/v6#egg=python-bps-6',
+        'python-bps>=6 @ https://github.com/tcprescott/python-bps/tarball/v6#egg=python-bps-6',
         'slugid'
     ]
 )
