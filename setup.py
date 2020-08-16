d98afc6d4445a3d2055816cf17d473a48368b5e8
Thomas Prescott <tcprescott@gmail.com>
2020-08-15 23:05:38 -0500

    Bump version: 5.2.6 → 5.2.7

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ README = (HERE / "README.md").read_text()
 
 setup(
     name="pyz3r",
-    version="5.2.6",
+    version="5.2.7",
     author="Thomas Prescott",
     author_email="tcprescott@gmail.com",
     description="A python module for interacting with the web API of various randomizers, such as https://alttpr.com or https://samus.link.",
