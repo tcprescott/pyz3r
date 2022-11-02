436e6f210ecd656ec286182f879b96f4243f1d2a
Thomas Prescott <tcprescott@gmail.com>
2022-11-02 14:44:31 -0500

    bump version

--- pyz3r/__init__.py
+++ pyz3r/__init__.py
@@ -7,8 +7,8 @@ A basic wrapper for the ALTTPR.com API.
 __title__ = 'pyz3r'
 __author__ = 'Thomas Prescott'
 __license__ = 'Apache 2.0'
-__copyright__ = 'Copyright 2018-2019 Thomas Prescott'
-__version__ = '6.0.3'
+__copyright__ = 'Copyright 2018-2022 Thomas Prescott'
+__version__ = '6.0.4'
 
 from pyz3r.alttpr import ALTTPR
 from pyz3r.sm import sm
--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ README = (HERE / "README.md").read_text()
 
 setup(
     name="pyz3r",
-    version="6.0.3",
+    version="6.0.4",
     author="Thomas Prescott",
     author_email="tcprescott@gmail.com",
     description="A python module for interacting with the web API of various randomizers, such as https://alttpr.com or https://samus.link.",
