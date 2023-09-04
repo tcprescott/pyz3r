461a2d311bfd7971af54b3b9ae8a89d6a0a317c9
Thomas Prescott <tcprescott@gmail.com>
2023-09-04 12:51:28 -0500

    Bump version: 6.0.9 → 6.1.0

--- pyz3r/__init__.py
+++ pyz3r/__init__.py
@@ -8,7 +8,7 @@ __title__ = 'pyz3r'
 __author__ = 'Thomas Prescott'
 __license__ = 'Apache 2.0'
 __copyright__ = 'Copyright 2018-2022 Thomas Prescott'
-__version__ = '6.0.9'
+__version__ = '6.1.0'
 
 from pyz3r.alttpr import ALTTPR
 from pyz3r.sm import sm
